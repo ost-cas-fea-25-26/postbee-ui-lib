adc723853bc9661cedef80c5e9945f8566a164de
Vittorio Costantini <vco@mailbox.org>
2025-10-11 23:37:27 +0200

    feat(typography): add typography components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Button, type ButtonProps, type ButtonVariant, type ButtonSize } from './Button';
+export * from './typography/heading';
+export * from './typography/label';
+export * from './typography/paragraph';
