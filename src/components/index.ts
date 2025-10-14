56b25cb86248e06b3f70cbb9fde62b6a419f23ba
Ricardo Coelho <devel@rmorgado.ch>
2025-10-14 08:23:56 +0200

    refactor(typography): improve imports/exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,2 @@
 export { Button, type ButtonProps, type ButtonVariant, type ButtonSize } from './Button';
-export * from './typography/heading';
-export * from './typography/label';
-export * from './typography/paragraph';
+export * from './typography';
