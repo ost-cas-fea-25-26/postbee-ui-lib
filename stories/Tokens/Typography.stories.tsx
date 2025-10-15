bc7e03cccbc05e5bd04d5a2540ae715620afacb0
Vittorio Costantini <vco@mailbox.org>
2025-10-15 21:05:12 +0200

    fix(knip): fix imports and exports that have been noticed by knip

--- stories/Tokens/Typography.stories.tsx
+++ stories/Tokens/Typography.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
 const textStyles = [
   { class: 'pb-h1', label: 'Heading 1' },
