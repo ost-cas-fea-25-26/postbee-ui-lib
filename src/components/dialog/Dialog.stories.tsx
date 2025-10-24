d4e803819e31fe87ba8f3bd1e5e7aba2e8049437
Ricardo Coelho <devel@rmorgado.ch>
2025-10-24 17:58:30 +0200

    fix: lint issues

--- src/components/dialog/Dialog.stories.tsx
+++ src/components/dialog/Dialog.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { useState } from 'react';
 import { Dialog } from './Dialog';
 import { Button } from '../buttons';
