2a61e062dd8f0553eaf075e17a0f18db089fc94c
Vittorio Costantini <vco@mailbox.org>
2025-10-24 16:20:45 +0200

    feat(buttons): add new component IconButton

---
+++ src/components/buttons/icon-button/IconButton.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { IconButton } from './IconButton';
+
+const meta = {
+  title: 'Components/Buttons/IconButton',
+  component: IconButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const IconButtonDefault: Story = {
+  args: {
+    icon: 'mumble',
+  },
+};
