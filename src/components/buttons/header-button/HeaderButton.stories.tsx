9b452ddd2b0c01b393ce92b83924ee217f80c111
Vittorio Costantini <vco@mailbox.org>
2025-10-25 21:05:10 +0200

    fix(stories/HeaderButton): fix import

--- src/components/buttons/header-button/HeaderButton.stories.tsx
+++ src/components/buttons/header-button/HeaderButton.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { HeaderButton } from './HeaderButton';
 import { fn } from 'storybook/test';
 
