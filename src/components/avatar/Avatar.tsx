f710b27e7ce1bbc6b5b831e62a2919f3e7cb8321
Vittorio Costantini <vco@mailbox.org>
2025-11-08 16:09:25 +0100

    fix(Avatar): fix editable button positions on smaller sizes

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 import * as RadixAvatar from '@radix-ui/react-avatar';
 import clsx from 'clsx';
 
-import { IconButton } from '../buttons';
+import { Button } from '../buttons';
+import { Icon } from '../icon';
 
 export interface AvatarProps extends React.ComponentPropsWithoutRef<typeof RadixAvatar.Root> {
   src?: string;
@@ -21,6 +22,13 @@ export const Avatar = React.forwardRef<HTMLSpanElement, AvatarProps>(
       md: 'w-16 h-16 text-base', // 64x64
       lg: 'w-24 h-24 text-lg', // 96x96
       xl: 'w-40 h-40 text-xl', // 160x160
+    }[size];
+
+    const editableButtonSizeClasses = {
+      sm: 'w-8 h-8 right-0 bottom-0',
+      md: 'w-8 h-8 right-2 bottom-2',
+      lg: 'right-3 bottom-4',
+      xl: 'bottom-4 right-4',
     }[size];
 
     return (
@@ -43,13 +51,14 @@ export const Avatar = React.forwardRef<HTMLSpanElement, AvatarProps>(
         </RadixAvatar.Root>
 
         {isEditable && (
-          <IconButton
-            icon="edit"
+          <Button
             aria-label="Edit avatar"
             variant="secondary"
             onClick={onEdit}
-            className={clsx('absolute bottom-4 right-4 translate-x-1/4 translate-y-1/4 !outline-0')}
-          />
+            className={clsx('rounded-full! absolute translate-x-1/4 translate-y-1/4 outline-0!', editableButtonSizeClasses)}
+          >
+            <Icon icon="edit" size={16} />
+          </Button>
         )}
       </div>
     );
