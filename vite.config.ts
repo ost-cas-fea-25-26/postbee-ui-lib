e3e7c6e29c4e381c267a2b430acbf60d1dd9211e
Vittorio Costantini <vco@mailbox.org>
2025-11-15 19:29:21 +0100

    fix(types): bundle all types in one file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest/config" />
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import dts from 'unplugin-dts/vite';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import svgr from 'vite-plugin-svgr';
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
@@ -35,6 +35,7 @@ export default defineConfig({
     }),
     dts({
       tsconfigPath: 'tsconfig.build.json',
+      rollupTypes: true,
     }),
   ],
   test: {},
