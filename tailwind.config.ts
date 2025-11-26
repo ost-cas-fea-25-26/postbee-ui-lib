0668a8c3dbdfca678be41337da5758def7ddabc7
Vittorio Costantini <vco@mailbox.org>
2025-11-26 22:25:45 +0100

    fix: remove obsolete tailwind.config.js

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './.storybook/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@radix-ui/themes/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: { extend: {} },
-};
